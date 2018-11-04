21e05c1985427ca45d98b1fb3f900cb61131f66f
Pasindu Madusanka <pasindusenerath@gmail.com>
2018-11-04 09:43:13 +0530

    Update resources/views/dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -41,47 +41,31 @@
                     </a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link" href="./user.html">
+                    <a class="nav-link" href="{{ asset('admin/user') }}">
                         <i class="material-icons">person</i>
                         <p>User Profile</p>
                     </a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link" href="./tables.html">
+                    <a class="nav-link" href="{{ asset('admin/table') }}">
                         <i class="material-icons">content_paste</i>
                         <p>Table List</p>
                     </a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link" href="./typography.html">
+                    <a class="nav-link" href="{{ asset('admin/typography') }}">
                         <i class="material-icons">library_books</i>
                         <p>Typography</p>
                     </a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link" href="./icons.html">
+                    <a class="nav-link" href="{{ asset('admin/icons') }}">
                         <i class="material-icons">bubble_chart</i>
                         <p>Icons</p>
                     </a>
                 </li>
-                <li class="nav-item ">
-                    <a class="nav-link" href="./map.html">
-                        <i class="material-icons">location_ons</i>
-                        <p>Maps</p>
-                    </a>
-                </li>
-                <li class="nav-item ">
-                    <a class="nav-link" href="./notifications.html">
-                        <i class="material-icons">notifications</i>
-                        <p>Notifications</p>
-                    </a>
-                </li>
-                <!-- <li class="nav-item active-pro ">
-                      <a class="nav-link" href="./upgrade.html">
-                          <i class="material-icons">unarchive</i>
-                          <p>Upgrade to PRO</p>
-                      </a>
-                  </li> -->
+
+             
             </ul>
         </div>
     </div>
