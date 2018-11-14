895c96b04c3dcc27d486d2f99805765eb15afd8c
Pasindu Madusanka <pasindusenerath@gmail.com>
2018-11-15 00:33:54 +0530

    Update resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -201,19 +201,7 @@
         <nav>
           <ul>
             <li>
-              <a href="https://www.creative-tim.com">
-                Creative Tim
-              </a>
-            </li>
-            <li>
-              <a href="http://presentation.creative-tim.com">
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="http://blog.creative-tim.com">
-                Blog
-              </a>
+            
             </li>
           </ul>
         </nav>
