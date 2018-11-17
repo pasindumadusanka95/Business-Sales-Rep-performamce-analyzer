e389792ce15edca8f178dde720fb920764329c94
Pasindu Madusanka <pasindusenerath@gmail.com>
2018-11-17 10:59:29 +0530

    updates and adding supplier table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
     <link rel="icon" type="image/png" href="../assets/img/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>
-        Material Dashboard by Creative Tim
+        Admin Dashboard
     </title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
     <!--     Fonts and icons     -->
@@ -29,7 +29,7 @@
       -->
         <div class="logo">
             <a href="http://www.creative-tim.com" class="simple-text logo-normal">
-                Creative Tim
+               DASHBOARD
             </a>
         </div>
         <div class="sidebar-wrapper">
